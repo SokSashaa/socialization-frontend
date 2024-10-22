dac76edf7ef95c1d5e3a3d0e60ad4e7198d099ec
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-10-22 11:11:05 +0300

    fix_components

--- src/components/DraggableList/DraggableList.tsx
+++ src/components/DraggableList/DraggableList.tsx
@@ -1,49 +1,59 @@
 import {
-  DndContext,
-  closestCorners,
-  useSensor,
-  useSensors,
-  PointerSensor,
-  TouchSensor,
+    closestCorners,
+    DndContext,
+    DragEndEvent,
+    PointerSensor,
+    TouchSensor,
+    useSensor,
+    useSensors,
 } from '@dnd-kit/core';
-import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
-import { restrictToParentElement } from '@dnd-kit/modifiers';
+import {SortableContext, verticalListSortingStrategy} from '@dnd-kit/sortable';
+import {restrictToParentElement} from '@dnd-kit/modifiers';
 import DraggableListItem from './DraggableListItem/DraggableListItem';
+import {FC, ReactNode} from "react";
 
-const DraggableList = ({
-  data,
-  classNameList,
-  renderItemContent,
-  onDragEnd,
-  withDragHandle = false,
-}) => {
-  const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+type DraggableListProps = {
+    data: any,
+    classNameList?: string,
+    renderItemContent: (item:any,index:any)=>ReactNode, //TODO разобраться с типом
+    onDragEnd: (event:DragEndEvent) => void , //TODO разобраться с типом
+    withDragHandle?: boolean,
+}
 
-  return (
-    <DndContext
-      sensors={sensors}
-      onDragEnd={onDragEnd}
-      collisionDetection={closestCorners}
-      modifiers={[restrictToParentElement]}
-    >
-      <ul className={classNameList}>
-        <SortableContext
-          items={data}
-          strategy={verticalListSortingStrategy}
+const DraggableList: FC<DraggableListProps> = ({
+                                                   data,
+                                                   classNameList,
+                                                   renderItemContent,
+                                                   onDragEnd,
+                                                   withDragHandle = false,
+                                               }) => {
+    const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+
+    return (
+        <DndContext
+            sensors={sensors}
+            onDragEnd={onDragEnd}
+            collisionDetection={closestCorners}
+            modifiers={[restrictToParentElement]}
         >
-          {data.map((item, index) => (
-            <DraggableListItem
-              key={item?.id || index}
-              index={item?.id || index}
-              withDragHandle={withDragHandle}
-            >
-              {renderItemContent && renderItemContent(item, index)}
-            </DraggableListItem>
-          ))}
-        </SortableContext>
-      </ul>
-    </DndContext>
-  );
+            <ul className={classNameList}>
+                <SortableContext
+                    items={data}
+                    strategy={verticalListSortingStrategy}
+                >
+                    {data.map((item, index) => (
+                        <DraggableListItem
+                            key={item?.id || index}
+                            index={item?.id || index}
+                            withDragHandle={withDragHandle}
+                        >
+                            {renderItemContent && renderItemContent(item, index)}
+                        </DraggableListItem>
+                    ))}
+                </SortableContext>
+            </ul>
+        </DndContext>
+    );
 };
 
 export default DraggableList;
--- src/modules/EditTest/components/TestEditor/TestEditor.tsx
+++ src/modules/EditTest/components/TestEditor/TestEditor.tsx
@@ -1,133 +1,133 @@
-import { Formik, FieldArray, Form } from 'formik';
-import { nanoid } from '@reduxjs/toolkit';
-import { toast } from 'react-toastify';
-import { arrayMove } from '@dnd-kit/sortable';
-import { PlusCircleIcon } from '@heroicons/react/24/solid';
-import { useEditTestMutation } from '../../api/editTestApiSlice';
-import { useGetTestQuery } from '../../../../app/api/common/testApiSlice';
+import {FieldArray, Form, Formik} from 'formik';
+import {nanoid} from '@reduxjs/toolkit';
+import {toast} from 'react-toastify';
+import {arrayMove} from '@dnd-kit/sortable';
+import {PlusCircleIcon} from '@heroicons/react/24/solid';
+import {useEditTestMutation} from '../../api/editTestApiSlice';
+import {useGetTestQuery} from '../../../../app/api/common/testApiSlice';
 
-import { Container, Button, ErrorMessage } from '../../../../UI';
-import { DraggableList } from '../../../../components';
+import {Button, Container, ErrorMessage} from '../../../../UI';
+import {DraggableList} from '../../../../components';
 import FormTop from '../FormTop/FormTop';
 import AddQuestionButton from '../AddQuestionButton/AddQuestionButton';
 import QuestionCard from '../QuestionCard/QuestionCard';
 
-import { testSchema } from '../../utils/validation.helper';
-import { onFieldArrayControl } from '../../utils/form.helper';
-import { transformTest, getQuestionPosition, transformResponse } from '../../utils/data.helper';
-import { INITIAL_QUESTION } from '../../utils/constants';
+import {testSchema} from '../../utils/validation.helper';
+import {onFieldArrayControl} from '../../utils/form.helper';
+import {getQuestionPosition, transformResponse, transformTest} from '../../utils/data.helper';
+import {INITIAL_QUESTION} from '../../utils/constants';
 import styles from './TestEditor.module.css';
 import Spinner from "../../../../UI/spinners/Spinner";
 import {DragEndEvent} from "@dnd-kit/core";
 import {useCallback} from "react";
 
-const TestEditor = ({ id }) => {
-  const { data: test, isLoading, isError } = useGetTestQuery(id);
-  const [editTest, { isLoading: isLoadingEdit }] = useEditTestMutation();
+const TestEditor = ({id}) => {
+    const {data: test, isLoading, isError} = useGetTestQuery(id);
+    const [editTest, {isLoading: isLoadingEdit}] = useEditTestMutation();
 
-  if (isLoading) {
-    return <Spinner typeSpinner={'big'} className="mt-10" />;
-  }
+    if (isLoading) {
+        return <Spinner typeSpinner={'big'} className="mt-10"/>;
+    }
 
-  if (isError) {
+    if (isError) {
+        return (
+            <ErrorMessage
+                message="Ошибка загрузки теста"
+                className="mt-10"
+            />
+        );
+    }
+
+    const upgradeTest = test && transformResponse(test);
+
+    const onDragEnd = useCallback((questions, setFieldValue) => (event: DragEndEvent) => {
+        const {active, over} = event;
+
+        if (active.id !== over?.id) {
+            const originalPos = getQuestionPosition(questions, active.id);
+            const finalPos = getQuestionPosition(questions, over?.id);
+
+            setFieldValue('questions', arrayMove(questions, originalPos, finalPos));
+        }
+    }, []);
+
+    const onSubmit = async (values) => {
+        try {
+            const transformedTest = transformTest(values);
+
+            await editTest(transformedTest);
+
+            toast.success('Тест сохранен');
+        } catch (error) {
+            toast.error(error?.data?.detail || 'Что-то пошло не так');
+        }
+    };
+
+    const submitBtnText = isLoadingEdit ? <Spinner typeSpinner={'mini'}/> : 'Сохранить';
+
     return (
-      <ErrorMessage
-        message="Ошибка загрузки теста"
-        className="mt-10"
-      />
-    );
-  }
+        <div className={styles.wrapper}>
+            <Container>
+                <div className={styles.inner}>
+                    <Formik
+                        initialValues={upgradeTest}
+                        onSubmit={onSubmit}
+                        validationSchema={testSchema}
+                    >
+                        {({values: testValues, handleSubmit, setFieldValue}) => (
+                            <Form
+                                method="post"
+                                className={styles.form}
+                            >
+                                <FormTop/>
+                                <FieldArray
+                                    name="questions"
+                                    render={(arrayHelpers) => {
+                                        const {questions} = testValues
 
-  const upgradeTest = test && transformResponse(test);
+                                        return questions && questions.length > 0 ? (
+                                            <DraggableList
+                                                data={questions}
+                                                onDragEnd={onDragEnd(questions, setFieldValue)}
+                                                classNameList={styles.questionList}
+                                                withDragHandle
+                                                renderItemContent={(item, index) => (
+                                                    <QuestionCard
+                                                        question={item}
+                                                        qIndex={index}
+                                                        arrayHelpers={arrayHelpers}
+                                                    />
+                                                )}
+                                            />
+                                        ) : (
+                                            <AddQuestionButton
+                                                onClick={onFieldArrayControl(arrayHelpers.push, {
+                                                    id: nanoid(),
+                                                    ...INITIAL_QUESTION,
+                                                })}
+                                                className="self-end"
+                                            >
+                                                <PlusCircleIcon className="icon h-8 w-8 fill-gray-500"/>
+                                            </AddQuestionButton>
+                                        );
+                                    }}
+                                />
 
-  const onDragEnd = (questions, setFieldValue) => (event: DragEndEvent) => {
-    const { active, over } = event;
-
-    if (active.id !== over?.id) {
-      const originalPos = getQuestionPosition(questions, active.id);
-      const finalPos = getQuestionPosition(questions, over?.id);
-
-      setFieldValue('questions', arrayMove(questions, originalPos, finalPos));
-    }
-  };
-
-  const onSubmit = async (values) => {
-    try {
-      const transformedTest = transformTest(values);
-
-      await editTest(transformedTest);
-
-      toast.success('Тест сохранен');
-    } catch (error) {
-      toast.error(error?.data?.detail || 'Что-то пошло не так');
-    }
-  };
-
-  const submitBtnText = isLoadingEdit ? <Spinner typeSpinner={'mini'} /> : 'Сохранить';
-
-  return (
-    <div className={styles.wrapper}>
-      <Container>
-        <div className={styles.inner}>
-          <Formik
-            initialValues={upgradeTest}
-            onSubmit={onSubmit}
-            validationSchema={testSchema}
-          >
-            {({ values: testValues, handleSubmit, setFieldValue }) => (
-              <Form
-                method="post"
-                className={styles.form}
-              >
-                <FormTop />
-                <FieldArray
-                  name="questions"
-                  render={(arrayHelpers) => {
-                    const { questions } = testValues
-
-                    return questions && questions.length > 0 ? (
-                      <DraggableList
-                        data={questions}
-                        onDragEnd={onDragEnd(questions, setFieldValue)}
-                        classNameList={styles.questionList}
-                        withDragHandle
-                        renderItemContent={(item, index) => (
-                          <QuestionCard
-                            question={item}
-                            qIndex={index}
-                            arrayHelpers={arrayHelpers}
-                          />
+                                <Button
+                                    className={styles.saveButton}
+                                    type="submit"
+                                    disabled={isLoadingEdit}
+                                    onClick={handleSubmit}
+                                >
+                                    {submitBtnText}
+                                </Button>
+                            </Form>
                         )}
-                      />
-                    ) : (
-                      <AddQuestionButton
-                        onClick={onFieldArrayControl(arrayHelpers.push, {
-                          id: nanoid(),
-                          ...INITIAL_QUESTION,
-                        })}
-                        className="self-end"
-                      >
-                        <PlusCircleIcon className="icon h-8 w-8 fill-gray-500" />
-                      </AddQuestionButton>
-                    );
-                  }}
-                />
-
-                <Button
-                  className={styles.saveButton}
-                  type="submit"
-                  disabled={isLoadingEdit}
-                  onClick={handleSubmit}
-                >
-                  {submitBtnText}
-                </Button>
-              </Form>
-            )}
-          </Formik>
+                    </Formik>
+                </div>
+            </Container>
         </div>
-      </Container>
-    </div>
-  );
+    );
 };
 
 export default TestEditor;
