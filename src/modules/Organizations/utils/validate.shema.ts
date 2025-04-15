a7a4df55d25f57c2862542034c901e17ed166290
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2025-04-15 12:01:29 +0300

    add modal to delete games

--- src/modules/Organizations/utils/validate.shema.ts
+++ src/modules/Organizations/utils/validate.shema.ts
@@ -1,10 +1,13 @@
-import {array, number, object, ObjectSchema, string} from "yup";
-import {organizations_dto} from "../../../dto/organizations.dto";
+import { object, string } from 'yup';
 
-export const organizationsValidate= object({
+export const organizationsValidate = object({
     name: string().trim().required('Обязательное поле'),
-    address:string().trim().required('Обязательное поле'),
-    site:string().trim().url('Введите верную ссылку').required('Обязательное поле'),
-    phone_number:string().trim().min(11, 'Номер минимум 11 символов').max(12, 'Номер не более 12 символов').required('Обязательное поле'),
-    email: string().email().trim().required('Обязательное поле'),
+    address: string().trim().required('Обязательное поле'),
+    site: string().trim().url('Введите верную ссылку').required('Обязательное поле'),
+    phone_number: string()
+        .trim()
+        .min(11, 'Номер минимум 11 символов')
+        .max(12, 'Номер не более 12 символов')
+        .required('Обязательное поле'),
+    email: string().email('Формат почты неверный').trim().required('Обязательное поле'),
 });
