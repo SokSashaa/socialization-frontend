61feef30c4f26559c3f62bf2941da04ac1c43fa3
Elijah <kudrin.i@autopiter.ru>
2025-05-28 23:54:27 +0300

    Update `ObservedList` component: change `user_id` type to `number`, adjust query for ordered results, and fix incorrect observeds access

--- src/modules/Profile/components/ObservedList/ObservedList.tsx
+++ src/modules/Profile/components/ObservedList/ObservedList.tsx
@@ -7,22 +7,22 @@ import ItemUser from '../ProfileInfoForm/ItemUser/ItemUser';
 import css from '../ProfileInfoForm/ProfileInfo.module.scss';
 
 interface ObservedListProps {
-	user_id: string;
+	user_id: number;
 }
 
 const ObservedList: FC<ObservedListProps> = ({user_id}) => {
-	const {data: observeds} = useGetObservedsByTutorQuery({id: user_id});
+	const {data: observeds} = useGetObservedsByTutorQuery({id: user_id, ordering: 'id'});
 
 	return (
 		<>
 			{observeds && (
 				<>
-					{observeds.length < 1 && <h3 className={css.title}>Наблюдаемых нет</h3>}
-					{observeds.length > 0 && (
+					{observeds.results.length < 1 && <h3 className={css.title}>Наблюдаемых нет</h3>}
+					{observeds.results.length > 0 && (
 						<>
 							<h3 className={css.title}>Назначенные наблюдаемые: </h3>
 							<div>
-								{observeds.map((item) => {
+								{observeds.results.map((item) => {
 									if (item) {
 										return <ItemUser key={item.id} user={item} />;
 									} else {
