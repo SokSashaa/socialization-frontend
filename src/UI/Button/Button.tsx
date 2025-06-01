06768fe500e05f5d059a7b31cb778b866f728cee
Elijah <kudrin.i@autopiter.ru>
2025-06-01 18:09:47 +0300

    Refactor `Button` component: format code, clean up prop destructuring, and enhance class handling with `clsx`.

--- src/UI/Button/Button.tsx
+++ src/UI/Button/Button.tsx
@@ -1,32 +1,25 @@
-import {forwardRef, HTMLAttributes} from "react";
-import clsx from "clsx";
-import styles from "./Button.module.scss";
+import React, {forwardRef, HTMLAttributes} from 'react';
+import clsx from 'clsx';
+
+import styles from './Button.module.scss';
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-    type?: 'button' | 'submit' | 'reset';
-    disabled?: boolean;
+	type?: 'button' | 'submit' | 'reset';
+	disabled?: boolean;
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
-                                                                  children,
-                                                                  onClick,
-                                                                  className,
-                                                                  type,
-                                                                  disabled
-                                                              }, ref) => {
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+	({children, onClick, className, type, disabled}, ref) => {
+		const classes = clsx('bg-main-yellow', styles.button, className);
 
-    const classes = clsx("bg-main-yellow", styles.button, className);
-    return (
-        <button
-            ref={ref}
-            className={classes}
-            type={type}
-            disabled={disabled}
-            onClick={onClick}
-        >
-            {children}
-        </button>
-    );
-});
+		return (
+			<button ref={ref} className={classes} type={type} disabled={disabled} onClick={onClick}>
+				{children}
+			</button>
+		);
+	}
+);
+
+Button.displayName = 'Button';
 
 export default Button;
